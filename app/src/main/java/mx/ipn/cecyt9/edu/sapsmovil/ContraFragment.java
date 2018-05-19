f92f6b69c8ccab8cc473d58b4b256ea4e03be871
AlexisDiego15 <redfoodiego@gmail.com>
2018-05-19 01:05:58 -0500

    4

---
+++ app/src/main/java/mx/ipn/cecyt9/edu/sapsmovil/ContraFragment.java
@@ -0,0 +1,21 @@
+package mx.ipn.cecyt9.edu.sapsmovil;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ContraFragment extends Fragment {
+    String em;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_contra, container, false);
+        ClienteMain activity = (ClienteMain) getActivity();
+        em = activity.getMyData();
+
+        return view;
+    }
+}
--- app/src/main/java/mx/ipn/cecyt9/edu/sapsmovil/CuentaFragment.java
+++ app/src/main/java/mx/ipn/cecyt9/edu/sapsmovil/CuentaFragment.java
@@ -1,27 +1,72 @@
 package mx.ipn.cecyt9.edu.sapsmovil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class CuentaFragment extends Fragment {
+public class CuentaFragment extends Fragment implements View.OnClickListener {
     String em;
+    Button msj, cam, camp, eli, cie;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cuenta, container, false);
+
         ClienteMain activity = (ClienteMain) getActivity();
         em = activity.getMyData();
 
-        /*muestraCo = (TextView) view.findViewById(R.id.txtcorreo);
-        muestraCo.setText(em);
-*/
+        msj = (Button)view.findViewById(R.id.mensaje);
+        cam = (Button)view.findViewById(R.id.cambio);
+        camp = (Button)view.findViewById(R.id.cambiop);
+        eli = (Button)view.findViewById(R.id.elimina);
+        cie = (Button)view.findViewById(R.id.cerrar);
 
-
+        msj.setOnClickListener(this);
+        cam.setOnClickListener(this);
+        camp.setOnClickListener(this);
+        eli.setOnClickListener(this);
+        cie.setOnClickListener(this);
 
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = null;
+        switch (v.getId()) {
+            case R.id.mensaje:
+                fragment = new MensajeFragment();
+                replaceFragment(fragment);
+                break;
+
+            case R.id.cambio:
+                fragment = new CambioFragment();
+                replaceFragment(fragment);
+                break;
+            case R.id.cambiop:
+                fragment = new ContraFragment();
+                replaceFragment(fragment);
+                break;
+            case R.id.elimina:
+                fragment = new EliminaFragment();
+                replaceFragment(fragment);
+                break;
+            case R.id.cerrar:
+                break;
+        }
+    }
+
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_fragment_placeholder, someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
