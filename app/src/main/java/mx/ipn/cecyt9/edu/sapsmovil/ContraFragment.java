00c9d9ed523ffa6290fb5f05abe72a7976046f31
AlexisDiego15 <redfoodiego@gmail.com>
2018-05-19 01:35:50 -0500

    4

--- app/src/main/java/mx/ipn/cecyt9/edu/sapsmovil/ContraFragment.java
+++ app/src/main/java/mx/ipn/cecyt9/edu/sapsmovil/ContraFragment.java
@@ -13,9 +13,9 @@ public class ContraFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contra, container, false);
+
         ClienteMain activity = (ClienteMain) getActivity();
         em = activity.getMyData();
-
         return view;
     }
 }
