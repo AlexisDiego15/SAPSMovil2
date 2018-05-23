b946d7cf72ccf68cbd12d6d49e0963cdb24e9b26
AlexisDiego15 <redfoodiego@gmail.com>
2018-05-23 01:51:35 -0500

    catalogo arreglo

--- app/src/main/java/mx/ipn/cecyt9/edu/sapsmovil/CatalogoFragment.java
+++ app/src/main/java/mx/ipn/cecyt9/edu/sapsmovil/CatalogoFragment.java
@@ -6,13 +6,61 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class CatalogoFragment extends Fragment {
 
+    ListView productos;
+    int[] IMAGES={R.drawable.naranja,R.drawable.naranja,R.drawable.naranja,R.drawable.naranja,R.drawable.naranja};
+    String[] NAMES={"naranja","naranja","naranja","naranja","naranja"};
+    String[] PRECIO={"12.50","12.50","12.50","12.50","12.50"};
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_catalogo, container, false);
+        View view = inflater.inflate(R.layout.fragment_catalogo, container, false);
 
+        productos = (ListView) view.findViewById(R.id.catalogo);
+        CustoAdapter custoAdapter = new CustoAdapter();
+        productos.setAdapter(custoAdapter);
+
+    return view;
     }
+
+    class CustoAdapter extends BaseAdapter{
+
+
+        @Override
+        public int getCount() {
+            return IMAGES.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.producto, null);
+
+            ImageView imageView = (ImageView) view.findViewById(R.id.imageView2);
+            TextView nombre = (TextView) view.findViewById(R.id.nombre_prod);
+            TextView precio = (TextView) view.findViewById(R.id.precio_prod);
+
+            imageView.setImageResource(IMAGES[i]);
+            nombre.setText(NAMES[i]);
+            precio.setText(PRECIO[i]);
+            return view;
+        }
+    }
+
 
 }
--- app/src/main/java/mx/ipn/cecyt9/edu/sapsmovil/ClienteSQLite.java
+++ app/src/main/java/mx/ipn/cecyt9/edu/sapsmovil/ClienteSQLite.java
@@ -18,7 +18,11 @@ public class ClienteSQLite extends SQLiteOpenHelper {
         bd.execSQL("insert into admin values(null,'Joel','Angeles','admin','admin')");
         bd.execSQL("create table mensaje(id_men integer primary key autoincrement, remitente text, cmensaje text, respuesta text)");
         bd.execSQL("create table productos(id_pro integer primary key autoincrement, nombrep text, precio integer, tipo text, descripcion text, cantidad integer)");
-       /*bd.execSQL("insert into productos values(null, 'bistek', 120, 'carnes', 'carne fresca', 120)");
+        bd.execSQL("create table carrito(id_pro integer, nombrep text, precio integer, tipo text, descripcion text, cantidad integer, usuario text, id_compra)");
+        bd.execSQL("create table compra(id_pro integer, nombrep text, precio integer, tipo text, descripcion text, cantidad integer, usuario text, id_compra)");
+        bd.execSQL("create table domicilio(calle text, colonia text, delegacion text, numero_int integer, numero_ext, ref text, usuario text, id_compra)");
+
+        /*bd.execSQL("insert into productos values(null, 'bistek', 120, 'carnes', 'carne fresca', 120)");
         bd.execSQL("insert into productos values(null, 'leche deslactosada', 9, 'lacteos', 'leche fresca', 200)");
         bd.execSQL("insert into productos values(null, 'melon', 12, 'frutas', 'fruta fresca', 218)");
         bd.execSQL("insert into productos values(null, 'jitomate', 18, 'verduras', 'verdura fresca', 172)");*/
@@ -29,9 +33,14 @@ public class ClienteSQLite extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase bd, int old, int nuevo) {
         bd.execSQL("create table cliente(id integer primary key autoincrement, nombre text, apellido text, correo text, usuario text, contra text, celular integer)");
         bd.execSQL("create table admin(id integer primary key autoincrement, nombre text, apellido text, usuario text, contra text)");
+        bd.execSQL("insert into admin values(null,'Joel','Angeles','admin','admin')");
         bd.execSQL("create table mensaje(id_men integer primary key autoincrement, remitente text, cmensaje text, respuesta text)");
         bd.execSQL("create table productos(id_pro integer primary key autoincrement, nombrep text, precio integer, tipo text, descripcion text, cantidad integer)");
-       /*bd.execSQL("insert into productos values(null, 'bistek', 120, 'carnes', 'carne fresca', 120)");
+        bd.execSQL("create table carrito(id_pro integer, nombrep text, precio integer, tipo text, descripcion text, cantidad integer, usuario text, id_compra)");
+        bd.execSQL("create table compra(id_pro integer, nombrep text, precio integer, tipo text, descripcion text, cantidad integer, usuario text, id_compra)");
+        bd.execSQL("create table domicilio(calle text, colonia text, delegacion text, numero_int integer, numero_ext, ref text, usuario text, id_compra)");
+
+        /*bd.execSQL("insert into productos values(null, 'bistek', 120, 'carnes', 'carne fresca', 120)");
         bd.execSQL("insert into productos values(null, 'leche deslactosada', 9, 'lacteos', 'leche fresca', 200)");
         bd.execSQL("insert into productos values(null, 'melon', 12, 'frutas', 'fruta fresca', 218)");
         bd.execSQL("insert into productos values(null, 'jitomate', 18, 'verduras', 'verdura fresca', 172)");*/
